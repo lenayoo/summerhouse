4e64fd3debc2c830dafb426259d6a1c5a2be3734
Lena Yoo <lenayoo17@gmail.com>
2023-07-23 15:56:03 +0900

    finish about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,15 @@ function About() {
   return (
     <>
       <Nav />
-      <div>About</div>
+      <div className="about-intro">
+        <img src="/public/main-4.JPG" alt="mom and niece" className="about" />
+        <div className="about-intro__text">
+          <p>
+            공기 좋고 물 맑은 누구나 (유집사의 허가를 미리 받은 사람이라면)
+            언제든 편히 쉴 수 있는 여주 2층집입니다.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
