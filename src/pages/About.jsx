76247769f30a8b360c8106063fbafa5bab5d2d95
Lena Yoo <lenayoo17@gmail.com>
2023-07-23 17:26:48 +0900

    add change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ function About() {
         <img src="/public/main-4.JPG" alt="mom and niece" className="about" />
         <div className="about-intro__text">
           <p>
-            공기 좋고 물 맑은 누구나{' '}
+            공기 좋고 물 맑은 누구나{'  '}
             <strong>(유집사의 허가를 미리 받은 사람이라면)</strong>
             언제든 편히 쉴 수 있는 여주 2층집입니다.
           </p>
