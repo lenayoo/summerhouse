ae5776955ae88199986602b0a6a45e1ecad27423
Lena Yoo <lenayoo17@gmail.com>
2023-07-23 17:16:55 +0900

    finish contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,23 @@
+import Nav from './Nav'
+
 function Contact() {
-  return <div>Contact</div>
+  return (
+    <>
+      <Nav />
+      <div className="contact">
+        <div>
+          <h3>You know who to contact to....</h3>
+          <p>
+            아래 사진의 사람들의 연락처를 모를 경우{' '}
+            <strong>lenayoo17@gmail.com</strong>으로 문의 바랍니다.
+          </p>
+        </div>
+        <div>
+          <img src="/public/contact2.JPG" className="contact-img" />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Contact
--- src/pages/Sightseeing.jsx
+++ src/pages/Sightseeing.jsx
@@ -1,5 +1,26 @@
+import Nav from './Nav'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+
 function Sightseeing() {
-  return <div>Sightseeing</div>
+  const center = {
+    lat: 37.275928,
+    lng: 127.630842,
+  }
+  return (
+    <>
+      <Nav />
+      <LoadScript
+        googleMapsApiKey="AIzaSyCiwtnT6hriZCfuOmh3-ZYAyIVwfzpmvKk" // 구글 지도 API 키
+      >
+        <GoogleMap
+          id="googleMap" // 지도를 보여줄 DOM 요소의 ID
+          mapContainerStyle={{ width: '100%', height: '400px' }} // 지도의 크기 설정
+          center={center} // 지도의 초기 중심 위치 설정
+          zoom={13} // 지도의 초기 줌 레벨 설정
+        />
+      </LoadScript>
+    </>
+  )
 }
 
 export default Sightseeing
